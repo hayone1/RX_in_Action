5ea3197f001ed9e41520a264e031c344471f71ad
Boluwatife Morawo <boluwatife.morawo@stu.cu.edu.ng>
2022-08-10 07:55:16 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// var numbers = new NumbersObservable(5);
+// var subscription = numbers.SubscribeConsole("numbers");
+
+// ChatClient chatClient = new();
+// var connection = chatClient.Connect("guest", "guest");
+
+// IObservable<string> observableConnection = new ObservableConnection(connection);
+
+// var subscription = observableConnection.SubscribeConsole("receiver");
+// chatClient.Connect("guest", "pa$$w0rd")
+//             .ToObservable()
+//             .SubscribeConsole("receiver");
+
+// var deferredMessages = chatClient.ObserveMessagesDeferred("user", "password");
+// var sub1 = deferredMessages.SubscribeConsole("ok");
+// var sub2 = deferredMessages.SubscribeConsole("yo");
+
+MockWPF EventPatternTest = new();
+EventPatternTest.scanner.OnWiFiFound();
+
+EventPatternTest.scanner.OnExWiFiFOund();
+EventPatternTest.scanner.OnConnected();
+
+
+var _toObservable = new EnumToObservable();
+//_toObservable.myobservable.SubscribeConsole("get the names");
+// _toObservable.NumbersAndThrow().ToObservable().SubscribeConsole("will_Throw_Error");
+//_toObservable.names.Subscribe(new ConsoleObserver<string>("Subscribe Directly"));
+_toObservable.ConcatMessages();
+//_toObservable.ObservableToEnumerable();
+//_toObservable.ObservableToList();
+_toObservable.ObservableToDict();
+_toObservable.ObservableToLookup();
+_toObservable.GenerateNUmbers();
+
+
+
